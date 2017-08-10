46aa286962ef057fb1410c6ba287c6a003155c82
Yattaze <arthurhuia@gmail.com>
2017-08-10 23:01:52 +1200

    Add files via upload

--- enemies.py
+++ enemies.py
@@ -1,8 +1,12 @@
+from inventory import inventory
+import items
+
 class enemy(object):
     def __init__(self, name, desc, hp):
         self.name = name
         self.desc = desc
         self.hp = hp
+        self.inv = inventory()
 
     def __str__(self):
         return 'Name: ' + self.name + '\nDescription: ' + self.desc + '\nHp: ' + str(self.hp)
@@ -10,5 +14,6 @@ class enemy(object):
 class cavebat(enemy):
     def __init__(self):
         super(cavebat, self).__init__('Cave Bat', 'Small annoying creature ~ kinda like abe', 10)
+        self.inv.add(items.gold, 1)
 
 cavebat = cavebat()
